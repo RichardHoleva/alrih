ac2b09c3767015074723cfca0dbb9a527c90b7e9
Richie <eaa24rhol@students.eaaa.dk>
2026-05-18 20:35:50 +0200

    splitting css into more tahn using one main css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './global.css'
+import './styles/global.css'
+import './styles/navbar.css'
+import './styles/hero.css'
+import './styles/about.css'
+import './styles/featured.css'
+import './styles/services.css'
+import './styles/process.css'
+import './styles/reviews.css'
+import './styles/footer.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
